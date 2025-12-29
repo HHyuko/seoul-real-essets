af38b79101d20020696d26706f25ae51e3dc1c14
HHyuko <hyuk.h2o@gmail.com>
2025-12-30 00:19:15 +0900

    Add files via upload

---
+++ plot.js
@@ -0,0 +1,13 @@
+// IMPORTANT
+// 이 파일은 자리표시자입니다.
+// 기존 PC에서 사용하던 plot.js를
+// 이 파일명(plot.js)으로 그대로 교체하세요.
+//
+// 조건:
+// Plotly.newPlot(
+//   "fbb5ddcb-85fb-45cc-aa94-644009d70ae9",
+//   data,
+//   layout
+// );
+//
+// 위 ID가 반드시 일치해야 합니다.
